fa04d5deec7dbc79187aa76b9fa3d1a035e06870
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2025-11-23 03:28:44 +0100

    usunięcie zbędnej funkcji
    przebudowana i naprawiona została przeniesiona do polecajek

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -1,21 +1,3 @@
-function sleep(miliseconds) {
-  var currentTime = new Date().getTime();
-  while (currentTime + miliseconds >= new Date().getTime()) {}
-}
-
-sleep(300);
-
-while (document.querySelector(".loading666")) {
-  Loading = document.querySelector(".loading666");
-  Loading.parentNode.removeChild(Loading);
-}
-
-while (document.querySelector(".ukryty")) {
-  Ukryty = document.querySelector(".ukryty");
-  Ukryty.classList.remove("hidden");
-  Ukryty.classList.remove("ukryty");
-}
-
 (!(function (t, e) {
   "function" == typeof define && define.amd
     ? define("jquery-bridget/jquery-bridget", ["jquery"], function (i) {
